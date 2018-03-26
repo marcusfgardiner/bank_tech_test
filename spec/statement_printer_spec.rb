c859d819aaabb21fab060c7ec0cba9839a221be7
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-26 17:31:15 +0100

    :white_check_mark: test: added file and test for statement_printer - initial print test

---
+++ spec/statement_printer_spec.rb
@@ -0,0 +1,10 @@
+require 'statement_printer'
+
+describe StatementPrinter do
+  let(:credit) { [50, "26/03/2018", "credit"] }
+  describe '#print_transaction' do
+    it 'prints out a credit transaction' do
+      expect { print_transaction(credit) }.to output("26/03/2018 || 50.00").to_stdout
+    end
+  end
+end

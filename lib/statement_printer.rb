7117c7a6de2c0d75d855e6339f2829834cd6e90e
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-26 18:05:10 +0100

    :recycle: refactor: extracted transaction type to make code more declarative

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,6 +1,6 @@
 class StatementPrinter
   def print_transaction(transaction)
-    if transaction[2] == 'credit'
+    if transaction_type(transaction) == 'credit'
       print transaction[1] + ' || ' + two_decimals(transaction[0])  + ' ||  || ' +  two_decimals(transaction[3])
     else
       print transaction[1] + ' ||  || ' + two_decimals(transaction[0]) +  ' || ' + two_decimals(transaction[3])
@@ -8,6 +8,10 @@ class StatementPrinter
   end
 
   private
+
+  def transaction_type(transaction)
+    transaction[2]
+  end
 
   def two_decimals(number)
     ("%0.02f" % number.to_f.to_s)

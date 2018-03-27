f69475580220c760ecf74b292ebd9172149aed7a
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-27 10:46:18 +0100

    :white_check_mark: test: added tests for withdraw to be called from extracted UI class

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,13 +2,22 @@ require 'bank_account'
 
 describe BankAccount do
 
-  it 'sends a payment confirmation text message' do
+  it 'deposits money using the transaction class' do
     expect(subject.transactions).to receive(:deposit).with(50, '12/12/2012')
     subject.deposit(50, '12/12/2012')
   end
 
   it 'puts to the screen that deposit was succesful' do
     expect { subject.deposit(50,'12/12/2012') }.to output("Deposit of 50 accepted\n").to_stdout
+  end
+
+  it 'withdraws money using the transaction class' do
+    expect(subject.transactions).to receive(:deposit).with(50, '12/12/2012')
+    subject.withdraw(50, '12/12/2012')
+  end
+
+  it 'puts to the screen that withdrawal was succesful' do
+    expect { subject.withdraw(50,'12/12/2012') }.to output("Withdrawal of 50 accepted\n").to_stdout
   end
 
   # it 'returns a string showing the deposit has happened' do
